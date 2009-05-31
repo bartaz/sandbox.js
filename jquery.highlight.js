bde1ccaa56ac093f4fd7c048d3889d9b9a1ee8f0
Bartek Szopka <bartek.szopka+github@gmail.com>
2009-05-31 13:41:28 +0200

    jslint now says it's fine

--- jquery.highlight.js
+++ jquery.highlight.js
@@ -44,54 +44,58 @@
  */
 
 jQuery.extend({
-	highlight: function(node, re, nodeName, className) {
-		if (node.nodeType === 3) {
-			var match = node.data.match(re);
-			if (match) {
-				var highlight = document.createElement(nodeName || 'span');
-				highlight.className = className || 'highlight';
-				var wordNode = node.splitText(match.index);
-				wordNode.splitText(match[0].length);
-				var wordClone = wordNode.cloneNode(true);
-				highlight.appendChild(wordClone);
-				wordNode.parentNode.replaceChild(highlight, wordNode);
-				return 1; //skip added node in parent
-			}
-		} else if ((node.nodeType === 1 && node.childNodes) // only element nodes that have children
-				&& !/(script|style)/i.test(node.tagName) // ignore script and style nodes
-				&& !(node.tagName === nodeName.toUpperCase() && node.className === className)) { // skip if already highlighted
-			for (var i = 0; i < node.childNodes.length; ++i) {
-				i += jQuery.highlight(node.childNodes[i], re, nodeName, className);
-			}
-		}
-		return 0;
-	}
+    highlight: function (node, re, nodeName, className) {
+        if (node.nodeType === 3) {
+            var match = node.data.match(re);
+            if (match) {
+                var highlight = document.createElement(nodeName || 'span');
+                highlight.className = className || 'highlight';
+                var wordNode = node.splitText(match.index);
+                wordNode.splitText(match[0].length);
+                var wordClone = wordNode.cloneNode(true);
+                highlight.appendChild(wordClone);
+                wordNode.parentNode.replaceChild(highlight, wordNode);
+                return 1; //skip added node in parent
+            }
+        } else if ((node.nodeType === 1 && node.childNodes) && // only element nodes that have children
+                !/(script|style)/i.test(node.tagName) && // ignore script and style nodes
+                !(node.tagName === nodeName.toUpperCase() && node.className === className)) { // skip if already highlighted
+            for (var i = 0; i < node.childNodes.length; i++) {
+                i += jQuery.highlight(node.childNodes[i], re, nodeName, className);
+            }
+        }
+        return 0;
+    }
 });
 
-jQuery.fn.removeHighlight = function(options) {
-	var settings = { className: 'highlight', element: 'span' };
-	jQuery.extend(settings, options);
+jQuery.fn.removeHighlight = function (options) {
+    var settings = { className: 'highlight', element: 'span' };
+    jQuery.extend(settings, options);
 
-	return this.find(settings.element + "." + settings.className).each(function() {
-		var parent = this.parentNode;
-		parent.replaceChild(this.firstChild, this);
-		parent.normalize();
-	}).end();
+    return this.find(settings.element + "." + settings.className).each(function () {
+        var parent = this.parentNode;
+        parent.replaceChild(this.firstChild, this);
+        parent.normalize();
+    }).end();
 };
 
-jQuery.fn.highlight = function(words, options) {
-	var settings = { className: 'highlight', element: 'span', ignoreCase: false, wordsOnly: false };
-	jQuery.extend(settings, options);
-	
-	if (words.constructor === String) { words = words.split(/\s+/); }
+jQuery.fn.highlight = function (words, options) {
+    var settings = { className: 'highlight', element: 'span', ignoreCase: false, wordsOnly: false };
+    jQuery.extend(settings, options);
+    
+    if (words.constructor === String) {
+        words = words.split(/\s+/);
+    }
 
-	var flag = settings.ignoreCase ? "i" : "";
-	var pattern = "(" + words.join("|") + ")";
-	if (settings.wordsOnly) {
-		pattern = "\\b" + pattern + "\\b";
-	}
-	var re = new RegExp(pattern, flag);
-	
-	return this.each(function(){ jQuery.highlight(this, re, settings.element, settings.className );});
+    var flag = settings.ignoreCase ? "i" : "";
+    var pattern = "(" + words.join("|") + ")";
+    if (settings.wordsOnly) {
+        pattern = "\\b" + pattern + "\\b";
+    }
+    var re = new RegExp(pattern, flag);
+    
+    return this.each(function () {
+        jQuery.highlight(this, re, settings.element, settings.className);
+    });
 };
 
--- specify.js
+++ specify.js
@@ -11,107 +11,124 @@
  *
  */
 
-(function(){
+(function () {
 
-// simple object extend function for internal use
-function extend(target, source){
-	for(var name in source){
-		if (source[name] !== undefined)
-			target[name] = source[name];
-	}
-	return target;
-}
+    // simple object extend function for internal use
+    function extend(target, source) {
+        for (var name in source) {
+            if (source[name] !== undefined) {
+                target[name] = source[name];
+            }
+        }
+        return target;
+    }
 
-specify = window.specify = function(name, specs) {
-	specify.runner.runGroup(name, specs);
-};
+    var specify = window.specify = function (name, specs) {
+        specify.runner.runGroup(name, specs);
+    };
 
-extend(specify, {
-	pass: function() {
-		throw { status : "passed", message : "passed" }
-	},
-	fail: function(message){
-		throw { status : "failed", message : message || "failed" }
-	},
-	pending: function(message){
-		throw { status : "pending", message : message || "is pending" }
-	},
+    extend(specify, {
+        pass: function () {
+            throw { status : "passed", message : "passed" };
+        },
+        fail: function (message) {
+            throw { status : "failed", message : message || "failed" };
+        },
+        pending: function (message) {
+            throw { status : "pending", message : message || "is pending" };
+        },
 
-	equal: function(value, expected, message){
-		if(value !== expected){
-			this.fail(message || "expected " + expected + " but got " + value);
-		}
-	},
-	unequal: function(value, unexpected, message){
-		if(value === unexpected){
-			this.fail(message || "nobody expects the Spanish Inq.. " + unexpected + " value but got it");
-		}
-	}
-});
+        equal: function (value, expected, message) {
+            if (value !== expected) {
+                this.fail(message || "expected " + expected + " but got " + value);
+            }
+        },
+        unequal: function (value, unexpected, message) {
+            if (value === unexpected) {
+                this.fail(message || "nobody expects the Spanish Inq.. " + unexpected + " value but got it");
+            }
+        }
+    });
 
-specify.log = {
-	passed: function(){ console.info.apply(this, arguments); },
-	failed: function(){ console.error.apply(this, arguments); },
-	pending: function(){ console.warn.apply(this, arguments); },
-	group: function(){ console.group.apply(this, arguments); },
-	groupEnd: function(){ console.groupEnd.apply(this, arguments); }
-};
+    specify.log = {
+        passed: function () {
+            console.info.apply(this, arguments);
+        },
+        failed: function () {
+            console.error.apply(this, arguments);
+        },
+        pending: function () {
+            console.warn.apply(this, arguments);
+        },
+        group: function () {
+            console.group.apply(this, arguments);
+        },
+        groupEnd: function () {
+            console.groupEnd.apply(this, arguments);
+        }
+    };
 
-specify.runner = {
-	runGroup: function(name, tests){
-		specify.log.group(name)
+    specify.runner = {
+        runGroup: function (name, specs) {
+            specify.log.group(name);
+            
+            for (var spec in specs) {
+                if (specs.hasOwnProperty(spec)) {
+                    this.run(spec, specs[spec]);
+                }
+            }
+            
+            specify.log.groupEnd();
+        },
 
-		for(var test in tests){
-			this.run(test, tests[test]);
-		}
-		specify.log.groupEnd();
-	},
+        run: function (name, test) {
+            try {
+                test.apply(specify.expectation);
+                specify.pass();
+            } catch (result) {
+                if (result.status) {
+                    specify.log[result.status](name, "--", test, result.message);
+                } else {
+                    throw result;
+                }
+            }
+        }
+    };
 
-	run: function(name, test){
-		try {
-			test.apply(specify.expectation);
-			specify.pass();
-		} catch (result) {
-			if(result.status){
-				specify.log[result.status](name, "--", test, result.message);
-			} else {
-				throw result;
-			}
-		}
-	}
-}
+    specify.expectation = function (value) {
+        return {
+            should_be: function (expected, message) {
+                return specify.equal(value, expected, message);
+            },
 
-specify.expectation = function(value){
-	return {
-		should_be: function(expected, message){
-			return specify.equal(value, expected, message);
-		},
+            should_not_be: function (unexpected, message) {
+                return specify.unequal(value, unexpected, message);
+            },
 
-		should_not_be: function(unexpected, message){
-			return specify.unequal(value, unexpected, message);
-		},
+            should_be_a: function (expectedType, message) {
+                if (expectedType.constructor === Function) {
+                    value = value.constructor;
+                } else {
+                    value = typeof value;
+                }
+                return specify.equal(value, expectedType, message);
+            },
 
-		should_be_a: function(expectedType, message){
-			if(expectedType.constructor === Function) value = value.constructor
-			else value = typeof value;
-			return specify.equal(value, expectedType, message);
-		},
+            should_be_true: function (message) {
+                return specify.equal(!!value, true, message || "expected " + value + " to be true");
+            }
+        };
+    };
 
-		should_be_true: function(message){
-			return specify.equal(!!value, true, message || "expected " + value + " to be true");
-		}
-	}
-}
+    extend(specify.expectation, {
+        pass: specify.pass,
+        is_ok: specify.pass,
 
-extend(specify.expectation, {
-	pass: specify.pass,
-	is_ok: specify.pass,
+        fail: specify.fail,
+        is_not_ok: specify.fail,
 
-	fail: specify.fail,
-	is_not_ok: specify.fail,
+        pending: specify.pending,
+        is_pending: specify.pending
+    });
 
-	pending: specify.pending,
-	is_pending: specify.pending,
-});
-
-})();
+}());
